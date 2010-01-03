6151cf79f7f8c55027791431c4949b5ae0de874a
Pedro Martins <pedromartins.pt@gmail.com>
2010-01-03 02:22:30 +0000

    Wireframe cube with pure GLUT
    Ignore-this: d72329699c5c9087ef02ba59605cbdf8
    
    darcs-hash:20100103022230-a91b7-44f4dc44eea23ea5a98235c834eec0ff59a21bd7.gz

--- Main.hs
+++ Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+import Graphics.UI.GLUT
+
+data Point3D = P3D { x :: Integer, y :: Integer, z :: Integer }
+
+data Level = Level { size :: Integer, obstacles :: [Point3D] }
+
+
 -- | main
 main :: IO ()
-main = putStrLn "Hello World" 
+main = do
+    getArgsAndInitialize
+    createWindow "Anaglyph Cube"
+    matrixMode $= Projection
+    loadIdentity
+    perspective 45.0 1 1 1000
+    matrixMode $= Modelview 0
+    displayCallback $= do 
+        clear [ ColorBuffer ]
+        translate $ Vector3 (0 :: Float) 0 (-5)
+        renderObject Wireframe (Cube 1)
+        flush
+    mainLoop
