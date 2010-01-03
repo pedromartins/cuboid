7b07f770b498ee99c8cdcf5a36ebfd03e6341ca1
Pedro Martins <pedromartins.pt@gmail.com>
2010-01-03 20:13:42 +0000

    Yampa based implementation
    Ignore-this: eaf91e37c5fcf7fb4109c8310eba0801
    
    darcs-hash:20100103201342-a91b7-c76b16e86e126d03ea413135ed6cea75b141b039.gz

---
+++ GLAdapter.hs
@@ -0,0 +1,21 @@
+module GLAdapter where
+
+import Graphics.UI.GLUT
+
+-- Copied from reactive-glut
+resizeScene :: Size -> IO ()
+resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
+resizeScene s@(Size width height) = do
+  -- putStrLn "resizeScene"
+  viewport   $= (Position 0 0, s)
+  matrixMode $= Projection
+  loadIdentity
+  perspective 75 (w2/h2) 0.001 100
+  matrixMode $= Modelview 0
+  flush
+ where
+   w2 = half width
+   h2 = half height
+   half z = realToFrac z / 2
+
+
--- Main.hs
+++ Main.hs
@@ -1,11 +1,20 @@
 module Main where
 
-import FRP.Reactive
-import FRP.Reactive.GLUT.Adapter as RG
+import FRP.Yampa
 import Graphics.UI.GLUT
 
-import Control.Applicative
+import Data.IORef
+import Control.Arrow
 import Data.Monoid
+
+import GLAdapter
+
+-- | Event Definition:
+
+data Input = Keyboard { key       :: Key,
+                        keyState  :: KeyState,
+                        modifiers :: Modifiers }
+-- | Rendering Code:
 
 data Point3D = P3D { x :: Integer, y :: Integer, z :: Integer }
 
@@ -13,47 +22,47 @@ data Level = Level { size :: Integer, obstacles :: [Point3D] }
 
 type R = Double
 
-resizeScene :: Size -> IO ()
-resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
-resizeScene s@(Size width height) = do
-  -- putStrLn "resizeScene"
-  viewport   $= (Position 0 0, s)
-  matrixMode $= Projection
-  loadIdentity
-  perspective 75 (w2/h2) 0.001 100
-  matrixMode $= Modelview 0
-  flush
- where
-   w2 = half width
-   h2 = half height
-   half z = realToFrac z / 2
-
 xAxis = Vector3 1 0 0 :: Vector3 R 
 yAxis = Vector3 0 1 0 :: Vector3 R
 zAxis = Vector3 0 0 1 :: Vector3 R 
 
-initGL :: IO ()
+initGL :: IO (Event Input)
 initGL = do
     getArgsAndInitialize
     createWindow "Hello World"
     clearColor $= Color4 0 0 0 0
     reshapeCallback $= Just resizeScene
+    return NoEvent
 
-sphere rot = do
+cube :: Double -> IO ()
+cube rot = do
     loadIdentity
     translate $ Vector3 (0 :: R) 0 (-2)
     rotate (rot * 100) yAxis
     renderObject Wireframe (Cube 1)
     flush
 
+game :: SF (Event Input) (IO ())
+game = time >>^ (\rot -> do 
+    clear [ ColorBuffer ]
+    cube rot
+    flush)
 
-justE' = (>>= maybe mempty return)
-
-rSphere :: UI -> Behavior Action
-rSphere ui = sphere <$> (countB $ justE' (fmap Just $ keyAction ui))
-
--- | main
+-- | Main, initializes Yampa and sets up reactimation loop
 main :: IO ()
-main = do 
-    initGL
-    adapt rSphere
+main = do
+    newInput <- newIORef NoEvent
+    rh <- reactInit initGL (\_ _ b -> b >> return False) game
+    displayCallback $= return ()
+    keyboardMouseCallback $= Just 
+        (\k ks m _ -> writeIORef newInput (Event $ Keyboard k ks m))
+    idleCallback $= Just (idle newInput rh) 
+    mainLoop
+
+-- | Reactimation iteration, supplying the input
+idle :: IORef (Event Input) -> ReactHandle (Event Input) (IO ()) -> IO ()
+idle newInput rh = do
+    newInput' <- readIORef newInput
+    react rh (1, Just newInput')
+    return ()
+    
