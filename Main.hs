d8a1da52f0ed30877424b3956aeaa22dc64b8daa
Pedro Martins <pedromartins.pt@gmail.com>
2010-01-03 22:23:20 +0000

    Added Level Rendering and Input Parsing
    Ignore-this: ee5a444778ebe70de025f444bcfdb2a9
    
    darcs-hash:20100103222320-a91b7-2fe8d008b01f0403a9ce7a052fd921482650e26b.gz

--- Main.hs
+++ Main.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE Arrows #-}
 module Main where
 
 import FRP.Yampa
-import Graphics.UI.GLUT
+import FRP.Yampa.Utilities
+import Graphics.UI.GLUT hiding (Level)
 
 import Data.IORef
 import Control.Arrow
@@ -18,9 +20,17 @@ data Input = Keyboard { key       :: Key,
 
 data Point3D = P3D { x :: Integer, y :: Integer, z :: Integer }
 
-data Level = Level { size :: Integer, obstacles :: [Point3D] }
+-- size is unncessary!!
+data Level = Level { obstacles :: [Point3D] }
+
+size :: Level -> Integer
+size = (+1) . maximum . map (\(P3D x y z) -> maximum [x,y,z]) . obstacles
+
+data GameState = Game { rotX :: Double, rotY :: Double }
 
 type R = Double
+
+testLevel = Level [P3D 0 0 0, P3D 5 5 5]
 
 xAxis = Vector3 1 0 0 :: Vector3 R 
 yAxis = Vector3 0 1 0 :: Vector3 R
@@ -29,30 +39,58 @@ zAxis = Vector3 0 0 1 :: Vector3 R
 initGL :: IO (Event Input)
 initGL = do
     getArgsAndInitialize
-    createWindow "Hello World"
+    createWindow "AnaCube!"
     clearColor $= Color4 0 0 0 0
     reshapeCallback $= Just resizeScene
     return NoEvent
 
-cube :: Double -> IO ()
-cube rot = do
+renderGame :: Level -> GameState -> IO ()
+renderGame l (Game rotX rotY) = do
     loadIdentity
-    translate $ Vector3 (0 :: R) 0 (-2)
-    rotate (rot * 100) yAxis
-    renderObject Wireframe (Cube 1)
+    translate $ Vector3 (0 :: R) 0 (-2*(fromInteger $ size l))
+    rotate (rotX * 10) xAxis
+    rotate (rotY * 10) yAxis
+    renderObject Wireframe (Cube $ fromInteger $ size l)
+    mapM_ renderObstacle $ obstacles l
     flush
+    where size2 :: R
+          size2 = (fromInteger $ size l)/2
+          renderObstacle p3D = preservingMatrix $ do
+            translate $ Vector3 (0.5 - size2 + (fromInteger $ x p3D)) 
+                                (0.5 - size2 + (fromInteger $ y p3D)) 
+                                (0.5 - size2 + (fromInteger $ z p3D))
+            renderObject Solid (Cube 1)
 
-game :: SF (Event Input) (IO ())
-game = time >>^ (\rot -> do 
-    clear [ ColorBuffer ]
-    cube rot
-    flush)
+keyDowns :: SF (Event Input) (Event Input)
+keyDowns = arr $ filterE ((==Down) . keyState)
+
+countHold :: SF (Event a) Integer
+countHold = count >>> hold 0
+
+game :: SF GameState (IO ())
+game = arr $ (\gs -> do
+        clear [ ColorBuffer ]
+        renderGame testLevel gs
+        flush)
+
+-- | Input
+parseInput :: SF (Event Input) GameState
+parseInput = proc i -> do
+    down <- keyDowns     -< i
+    ws   <- countKey 'w' -< down
+    as   <- countKey 'a' -< down
+    ss   <- countKey 's' -< down
+    ds   <- countKey 'd' -< down
+    returnA -< Game (fromInteger $ (ws - ss)) (fromInteger $ (ds - as))
+
+    where countKey c = filterE ((==(Char c)) . key) ^>> countHold
 
 -- | Main, initializes Yampa and sets up reactimation loop
 main :: IO ()
 main = do
     newInput <- newIORef NoEvent
-    rh <- reactInit initGL (\_ _ b -> b >> return False) game
+    rh <- reactInit initGL (\_ _ b -> b >> return False) 
+                    (parseInput >>> game)
     displayCallback $= return ()
     keyboardMouseCallback $= Just 
         (\k ks m _ -> writeIORef newInput (Event $ Keyboard k ks m))
