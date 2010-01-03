93ec2c8c1c04552025e76440c1acff828cc405b7
Pedro Martins <pedromartins.pt@gmail.com>
2010-01-03 13:46:02 +0000

    Reactive-based implementation (performance problems)
    Ignore-this: 82a4a22e0b3cd5ec67bdb09578a9f5c5
    
    darcs-hash:20100103134602-a91b7-a0bef33e1125c76bc21833b4a13c65f06bc74655.gz

--- Main.hs
+++ Main.hs
@@ -1,24 +1,59 @@
 module Main where
 
+import FRP.Reactive
+import FRP.Reactive.GLUT.Adapter as RG
 import Graphics.UI.GLUT
+
+import Control.Applicative
+import Data.Monoid
 
 data Point3D = P3D { x :: Integer, y :: Integer, z :: Integer }
 
 data Level = Level { size :: Integer, obstacles :: [Point3D] }
 
+type R = Double
+
+resizeScene :: Size -> IO ()
+resizeScene (Size w 0) = resizeScene (Size w 1) -- prevent divide by zero
+resizeScene s@(Size width height) = do
+  -- putStrLn "resizeScene"
+  viewport   $= (Position 0 0, s)
+  matrixMode $= Projection
+  loadIdentity
+  perspective 75 (w2/h2) 0.001 100
+  matrixMode $= Modelview 0
+  flush
+ where
+   w2 = half width
+   h2 = half height
+   half z = realToFrac z / 2
+
+xAxis = Vector3 1 0 0 :: Vector3 R 
+yAxis = Vector3 0 1 0 :: Vector3 R
+zAxis = Vector3 0 0 1 :: Vector3 R 
+
+initGL :: IO ()
+initGL = do
+    getArgsAndInitialize
+    createWindow "Hello World"
+    clearColor $= Color4 0 0 0 0
+    reshapeCallback $= Just resizeScene
+
+sphere rot = do
+    loadIdentity
+    translate $ Vector3 (0 :: R) 0 (-2)
+    rotate (rot * 100) yAxis
+    renderObject Wireframe (Cube 1)
+    flush
+
+
+justE' = (>>= maybe mempty return)
+
+rSphere :: UI -> Behavior Action
+rSphere ui = sphere <$> (countB $ justE' (fmap Just $ keyAction ui))
 
 -- | main
 main :: IO ()
-main = do
-    getArgsAndInitialize
-    createWindow "Anaglyph Cube"
-    matrixMode $= Projection
-    loadIdentity
-    perspective 45.0 1 1 1000
-    matrixMode $= Modelview 0
-    displayCallback $= do 
-        clear [ ColorBuffer ]
-        translate $ Vector3 (0 :: Float) 0 (-5)
-        renderObject Wireframe (Cube 1)
-        flush
-    mainLoop
+main = do 
+    initGL
+    adapt rSphere
