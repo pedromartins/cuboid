5697ff65a7993f43e7fa22464ff11411fc4aede3
Pedro Martins <pedromartins.pt@gmail.com>
2010-03-24 21:42:42 +0000

    Added a level and fixed cabal package
    Ignore-this: 6e33322c7ced43ec6b729a4b1ecbd63c
    Thanks to Thomas Branch for the level
    
    darcs-hash:20100324221655-a91b7-fdf975bba6c265e2c8501973b7c4b7b80a0e8462.gz

--- Game.hs
+++ Game.hs
@@ -39,6 +39,7 @@ data GameState = Game { level     :: Level,
 -- TODO: List can't be empty!
 testLevel = Level (P3D 0 0 1) (P3D 4 4 5) [P3D 0 0 0, P3D 0 5 1, P3D 5 4 1]
 testLevel2 = Level (P3D 0 0 1) (P3D 0 4 1) [P3D 5 5 5]
+testLevel3 = Level (P3D 0 1 0) (P3D 4 2 1)[P3D 0 1 1, P3D 1 2 0, P3D 1 3 3, P3D 1 1 4, P3D 2 3 0, P3D 3 1 0, P3D 3 4 0, P3D 3 0 2, P3D 3 3 3, P3D 3 1 4, P3D 4 2 0] 
 
-levels = concat (repeat [testLevel, testLevel2])
+levels = concat (repeat [testLevel, testLevel2, testLevel3])
 
