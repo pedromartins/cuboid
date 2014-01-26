05ad3523da01785849cf8fa984e0e6e2cb6dc20b
Miroslav Puda <pakanek@gmail.com>
2014-01-26 01:07:49 +0100

    Fix for flickering

--- Graphics.hs
+++ Graphics.hs
@@ -11,26 +11,26 @@ import Types
 import Utils
 
 -- Helpful OpenGL constants for rotation
-xAxis = G.Vector3 1 0 0 :: G.Vector3 R 
+xAxis = G.Vector3 1 0 0 :: G.Vector3 R
 yAxis = G.Vector3 0 1 0 :: G.Vector3 R
 zAxis = G.Vector3 0 0 1 :: G.Vector3 R
 
 initGL :: IO ()
 initGL = do
     getArgsAndInitialize
+    initialDisplayMode $= [ WithDepthBuffer, DoubleBuffered ]
     createWindow "Cuboid!"
-    initialDisplayMode $= [ WithDepthBuffer ]
     depthFunc          $= Just Less
     clearColor         $= Color4 0 0 0 0
     light (Light 0)    $= Enabled
-    lighting           $= Enabled 
-    lightModelAmbient  $= Color4 0.5 0.5 0.5 1 
+    lighting           $= Enabled
+    lightModelAmbient  $= Color4 0.5 0.5 0.5 1
     diffuse (Light 0)  $= Color4 1 1 1 1
     blend              $= Enabled
-    blendFunc          $= (SrcAlpha, OneMinusSrcAlpha) 
+    blendFunc          $= (SrcAlpha, OneMinusSrcAlpha)
     colorMaterial      $= Just (FrontAndBack, AmbientAndDiffuse)
     reshapeCallback    $= Just resizeScene
-    return () 
+    return ()
 
 -- Copied from reactive-glut
 resizeScene :: Size -> IO ()
@@ -42,7 +42,6 @@ resizeScene s@(Size width height) = do
   loadIdentity
   perspective 45 (w2/h2) 1 1000
   matrixMode $= Modelview 0
-  flush
  where
    w2 = half width
    h2 = half height
@@ -57,17 +56,16 @@ renderGame (Game l rotX pPos) = do
     -- TODO: calculate rotation axis based on rotX/Y
     rotate (rotX * 10) xAxis
     color $ Color3 (1 :: R) 1 1
-    position (Light 0) $= Vertex4 0 0 0 1  
+    position (Light 0) $= Vertex4 0 0 0 1
     renderObject Wireframe (Cube $ fromInteger $ size l)
     renderPlayer pPos
     renderGoal (p3DtoV3 $ endPoint l)
     mapM_ (renderObstacle . p3DtoV3) $ obstacles l
-    flush
     where size2 :: R
           size2 = (fromInteger $ size l)/2
           green  = Color4 0.8 1.0 0.7 0.9 :: Color4 R
           greenG = Color4 0.8 1.0 0.7 1.0 :: Color4 R
-          red    = Color4 1.0 0.7 0.8 1.0 :: Color4 R 
+          red    = Color4 1.0 0.7 0.8 1.0 :: Color4 R
           renderShapeAt s p = preservingMatrix $ do
             translate $ G.Vector3 (0.5 - size2 + vector3X p)
                                   (0.5 - size2 + vector3Y p)
@@ -75,12 +73,11 @@ renderGame (Game l rotX pPos) = do
             renderObject Solid s
           renderObstacle = (color green >>) . (renderShapeAt $ Cube 1)
           renderPlayer   = (color red >>) . (renderShapeAt $ Sphere' 0.5 20 20)
-          renderGoal     = 
-            (color greenG >>) . (renderShapeAt $ Sphere' 0.5 20 20) 
+          renderGoal     =
+            (color greenG >>) . (renderShapeAt $ Sphere' 0.5 20 20)
 
 draw :: SF GameState (IO ())
 draw = arr $ (\gs -> do
         clear [ ColorBuffer, DepthBuffer ]
         renderGame gs
-        flush)
-
+        swapBuffers)
